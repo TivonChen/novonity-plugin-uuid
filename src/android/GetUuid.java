b85b9ead15cc5750269675f8e081d39a10619f4a
TivonChen <malonely@gmail.com>
2016-10-10 10:21:45 +0800

    update android
    update android to cordova-plugin-wechat code

--- src/android/GetUuid.java
+++ src/android/GetUuid.java
@@ -3,58 +3,110 @@ package com.novonity.plugin.getdevice;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
+import org.apache.cordova.CordovaWebView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.Manifest;
 import android.content.Context;
-
-import java.util.UUID;
+import android.content.pm.PackageManager;
+import android.provider.Settings.Secure;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 
-import android.widget.Toast;
-
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
-import android.preference.PreferenceManager;
+import java.lang.reflect.Method;
 
 public class GetUuid extends CordovaPlugin {
 
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		if(action.equals("get")) {
-			SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(cordova.getActivity());
-			String uuid = sp.getString("uuid", "");
-			if(uuid != null && !uuid.equals("")){
-				try {
-					callbackContext.success(uuid);
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-			}else{
-				TelephonyManager tm = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
-                final String tmDevice, tmSerial, androidId;
-                tmDevice = "" + tm.getDeviceId();
-                tmSerial = "" + tm.getSimSerialNumber();
-                androidId = ""
-                        + android.provider.Settings.Secure.getString(
-                        cordova.getActivity().getContentResolver(),
-                        android.provider.Settings.Secure.ANDROID_ID);
+    public static final String TAG = "UniqueDeviceID";
+    public CallbackContext callbackContext;
+    public static final int REQUEST_READ_PHONE_STATE = 0;
 
-                UUID deviceUuid = new UUID(androidId.hashCode(),
-                        ((long) tmDevice.hashCode() << 32) | tmSerial.hashCode());
+    protected final static String permission = Manifest.permission.READ_PHONE_STATE;
 
-                String uniqueId = deviceUuid.toString();
-				Editor editor = sp.edit();
-				editor.putString("uuid",uniqueId);
-				editor.commit();
-				try {
-					callbackContext.success(uniqueId);
-				} catch (JSONException e) {
-					e.printStackTrace();
-				}
-			}
-			return true;
-		}
-		return false;
-	}
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        this.callbackContext = callbackContext;
+        try {
+            if (action.equals("get")) {
+                if(this.hasPermission(permission)){
+                    getDeviceId();
+                }else{
+                    this.requestPermission(this, REQUEST_READ_PHONE_STATE, permission);
+                }
+            }else {
+                this.callbackContext.error("Invalid action");
+                return false;
+            }
+        }catch(Exception e ) {
+            this.callbackContext.error("Exception occurred: ".concat(e.getMessage()));
+            return false;
+        }
+        return true;
+
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException {
+        if(requestCode == REQUEST_READ_PHONE_STATE){
+            getDeviceId();
+        }
+    }
+
+    protected void getDeviceId(){
+        try {
+            Context context = cordova.getActivity().getApplicationContext();
+            TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+            String uuid;
+            String androidID = Secure.getString(context.getContentResolver(), Secure.ANDROID_ID);
+            String deviceID = tm.getDeviceId();
+            String simID = tm.getSimSerialNumber();
+
+            if ("9774d56d682e549c".equals(androidID) || androidID == null) {
+                androidID = "";
+            }
+
+            if (deviceID == null) {
+                deviceID = "";
+            }
+
+            if (simID == null) {
+                simID = "";
+            }
+
+            uuid = androidID + deviceID + simID;
+            uuid = String.format("%32s", uuid).replace(' ', '0');
+            uuid = uuid.substring(0, 32);
+            uuid = uuid.replaceAll("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})", "$1-$2-$3-$4-$5");
+
+            this.callbackContext.success(uuid);
+        }catch(Exception e ) {
+            this.callbackContext.error("Exception occurred: ".concat(e.getMessage()));
+        }
+    }
+
+    private boolean hasPermission(String permission) throws Exception{
+        boolean hasPermission = true;
+        Method method = null;
+        try {
+            method = cordova.getClass().getMethod("hasPermission", permission.getClass());
+            Boolean bool = (Boolean) method.invoke(cordova, permission);
+            hasPermission = bool.booleanValue();
+        } catch (NoSuchMethodException e) {
+            Log.w(TAG, "Cordova v" + CordovaWebView.CORDOVA_VERSION + " does not support API 23 runtime permissions so defaulting to GRANTED for " + permission);
+        }
+        return hasPermission;
+    }
+
+    private void requestPermission(CordovaPlugin plugin, int requestCode, String permission) throws Exception{
+        try {
+            java.lang.reflect.Method method = cordova.getClass().getMethod("requestPermission", org.apache.cordova.CordovaPlugin.class ,int.class, java.lang.String.class);
+            method.invoke(cordova, plugin, requestCode, permission);
+        } catch (NoSuchMethodException e) {
+            throw new Exception("requestPermission() method not found in CordovaInterface implementation of Cordova v" + CordovaWebView.CORDOVA_VERSION);
+        }
+    }
 }
+
